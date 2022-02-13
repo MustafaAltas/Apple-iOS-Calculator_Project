a5206825b9308098a2e629298318a7d788480a01
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-02-13 20:37:08 +0300

    güncellendi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //****saat */
 
+//sol üst köşede saat'in görünmesi için yazılan kodlar.
+
 let saat = document.getElementById("saat");
 let dakika = document.getElementById("dakika");
 
@@ -17,4 +19,32 @@ if (new Date().getMinutes() < 10 && new Date().getMinutes() >= 0) {
     dakika.innerHTML = new Date().getMinutes();
 }
 
-//******** */
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++/
+
+//**RAKAMLARIN TANIMLANMASI */
+let sıfır = document.getElementById("sıfır");
+let bir = document.getElementById("bir");
+let iki = document.getElementById("iki");
+let üç = document.getElementById("üç");
+let dört = document.getElementById("dört");
+let beş = document.getElementById("beş");
+let altı = document.getElementById("altı");
+let yedi = document.getElementById("yedi");
+let sekiz = document.getElementById("sekiz");
+let dokuz = document.getElementById("dokuz");
+let nokta = document.getElementById("nokta");
+
+//**OPERATÖRLERİN TANIMLANMASI */
+const modülüs = document.getElementById("modülüs");
+const bölme = document.getElementById("bölme");
+const çarpı = document.getElementById("çarpı");
+const eksi = document.getElementById("eksi");
+const artı = document.getElementById("artı");
+const eşittir = document.getElementById("eşittir");
+const ac_tuş = document.getElementById("ac_tuş");
+
+
+
+
+
+
